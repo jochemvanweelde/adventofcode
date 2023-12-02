abc68d1a64cf213d05c991dc4bc2f95957e58ff2
Jochem van Weelde <jochem@heattransformers.com>
2023-12-02 09:49:57 +0100

    aoc2015 day 7 update

--- aoc2015/src/day07/index.ts
+++ aoc2015/src/day07/index.ts
@@ -2,9 +2,10 @@ import run from "aocrunner";
 
 const parseInput = (rawInput: string) => rawInput;
 
-type operation = string
 type wire = string
-const wireMap = new Map<wire, operation>()
+type signal = number
+type operation = string
+const wireMap = new Map<wire, signal>()
 
 function not(n: number){
   return ~n & 0xFFFF
@@ -22,48 +23,39 @@ function and(n: number, s: number){
   return n & s
 }
 
+function or(n: number, s: number){
+  return n | s
+}
 
 
-function solveOperation(operation: operation){
+function solveOperation(operation: operation, result_wire: wire): void {
   const parts = operation.split(' ')
   if (parts.length === 1){ //Direct value
     const value = parseInt(parts[0]);
-    if (!isNaN(value)){
-      return value
+    if (isNaN(value)){
+      throw new Error('Invalid operation')
     }
-    return solveOperation(wireMap.get(parts[0]) || '0')
+    wireMap.set(result_wire, value)
   }
   else if(parts.length === 2){ //NOT operator
     const value = parseInt(parts[1])
-    if (!isNaN(value)){
-      return not(value)
+    if (isNaN(value)){
+      throw new Error('Invalid operation')
     }
-    return solveOperation(wireMap.get(parts[0]) || '0')
+    wireMap.set(result_wire, not(value))
   }
   else if(parts.length === 3){
     const [value1, operator, value2] = parts
-
-    
-
   }
 }
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
   const instructions = input.split('\n')
-
-  // Create Wire Map
-  for (const instruction of instructions){
-    const [operation, wire] = instruction.split(' -> ')
-    wireMap.set(wire, operation)
-  }
-
-  // Solve Wire Map
-  const wireToSolve = wireMap.get('a')
-  if (!wireToSolve) throw Error('No wire a found!')
-
-  
-
+  instructions.forEach(instruction => {
+    const [operation, result_wire] = instruction.split(' -> ')
+    solveOperation(operation, result_wire);
+  })
   return;
 };
 
@@ -76,10 +68,18 @@ const part2 = (rawInput: string) => {
 run({
   part1: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+        123 -> x
+        456 -> y
+        x AND y -> d
+        x OR y -> e
+        x LSHIFT 2 -> f
+        y RSHIFT 2 -> a
+        NOT x -> h
+        NOT y -> i`,
+        expected: 114,
+      },
     ],
     solution: part1,
   },
