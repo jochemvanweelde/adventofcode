46a0cd72693009c1b33220a3c0ba31a198f6ec72
Jochem van Weelde <jochem@heattransformers.com>
2023-12-05 17:06:35 +0100

    Started day 5

---
+++ aoc2023/src/day05/types.ts
@@ -0,0 +1,17 @@
+export type Seed = number;
+
+export type Range = {
+  destination: number
+  source: number
+  length: number
+}
+
+export type ConvertMap = {
+  name: string
+  ranges: Range[]
+}
+
+export type FertilizerPlan = {
+  seeds: Seed[]
+  map: ConvertMap
+}
