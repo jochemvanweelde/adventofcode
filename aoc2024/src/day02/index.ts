dacfbb2f86742b01569feca1fe89974fccf8befa
Jochem van Weelde <jochem@heattransformers.com>
2024-12-02 18:02:17 +0100

    aoc 2024 day 2 part 2

--- aoc2024/src/day02/index.ts
+++ aoc2024/src/day02/index.ts
@@ -1,16 +1,22 @@
 import run from 'aocrunner';
+import { removeItemAtIndex } from '../utils/removeItemAtIndex.js';
 
 const parseInput = (rawInput: string) => rawInput;
 
 const convertInputToNumberArray = (input: string) =>
   input.split(' ').map(Number);
 
-const isFollowingRules = (numberArray: number[]) => {
+const isFollowingRules = (
+  numberArray: number[],
+  retryRecursive: boolean = false,
+) => {
   if (numberArray.length < 2) return true;
 
   let isIncreasing = false;
   let isDecreasing = false;
-  return numberArray.every((num, index, array) => {
+  let lastUsedIndex = 0;
+  const result = numberArray.every((num, index, array) => {
+    lastUsedIndex = index;
     if (index === array.length - 1) return true;
     if (num < array[index + 1]) {
       if (isDecreasing) return false;
@@ -23,6 +29,23 @@ const isFollowingRules = (numberArray: number[]) => {
     const distance = Math.abs(num - array[index + 1]);
     return distance >= 1 && distance <= 3;
   });
+
+  if (result) return true;
+  if (!retryRecursive) return false;
+
+  return (
+    (lastUsedIndex > 0 &&
+      isFollowingRules(
+        removeItemAtIndex(numberArray, lastUsedIndex - 1),
+        false,
+      )) ||
+    isFollowingRules(removeItemAtIndex(numberArray, lastUsedIndex), false) ||
+    (lastUsedIndex < numberArray.length &&
+      isFollowingRules(
+        removeItemAtIndex(numberArray, lastUsedIndex + 1),
+        false,
+      ))
+  );
 };
 
 const part1 = (rawInput: string) => {
@@ -30,7 +53,7 @@ const part1 = (rawInput: string) => {
 
   const arrays = input.split('\n').map(convertInputToNumberArray);
 
-  const validArrays = arrays.filter(isFollowingRules);
+  const validArrays = arrays.filter((array) => isFollowingRules(array, false));
 
   return validArrays.length;
 };
@@ -38,7 +61,11 @@ const part1 = (rawInput: string) => {
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return;
+  const arrays = input.split('\n').map(convertInputToNumberArray);
+
+  const validArrays = arrays.filter((array) => isFollowingRules(array, true));
+
+  return validArrays.length;
 };
 
 run({
@@ -58,10 +85,15 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`,
+        expected: 4,
+      },
     ],
     solution: part2,
   },
