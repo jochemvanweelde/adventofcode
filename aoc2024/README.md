67527ce7d800f364d87ed09b5d0e055f28831362
Jochem van Weelde <jochem@heattransformers.com>
2024-12-07 09:58:40 +0100

    aoc 2024 day 5 part 2

--- aoc2024/README.md
+++ aoc2024/README.md
@@ -19,7 +19,7 @@ Merry Christmas and happy coding! 🎅🎄🎁
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
 [![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%86/yellow)](src/day04)
-[![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%86/yellow)](src/day05)
+[![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
@@ -94,16 +94,16 @@ Both parts: 0.847ms
 
 ```
 Day 04
-Time part 1: 5.173ms
+Time part 1: 6.878ms
 Time part 2: -
-Both parts: 5.173ms
+Both parts: 6.878ms
 ```
 
 ```
 Day 05
-Time part 1: 6.13ms
-Time part 2: -
-Both parts: 6.13ms
+Time part 1: 3.465ms
+Time part 2: 23.123ms
+Both parts: 26.587ms
 ```
 
 ```
@@ -247,8 +247,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 8/50
-Total time: 18.708ms
+Total stars: 9/50
+Total time: 40.872ms
 ```
 
 <!--/RESULTS-->
--- aoc2024/src/day05/index.ts
+++ aoc2024/src/day05/index.ts
@@ -18,6 +18,19 @@ const isCorrectOrder = (orders: number[][], pages: number[]) =>
     return true;
   });
 
+const sortArrayBasedOnOrders = (orders: number[][], pages: number[]) =>
+  pages.sort((a, b) => {
+    const relevantOrder = orders.find(
+      (order) => order.includes(a) && order.includes(b),
+    );
+    if (relevantOrder) {
+      const indexOfFirstOrderPage = pages.indexOf(relevantOrder[0]);
+      const indexOfSecondOrderPage = pages.indexOf(relevantOrder[1]);
+      return indexOfFirstOrderPage - indexOfSecondOrderPage;
+    }
+    return 0;
+  });
+
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
@@ -32,8 +45,6 @@ const part1 = (rawInput: string) => {
     isCorrectOrder(orders, pages),
   );
 
-  console.log(correctPagesOrders);
-
   //get middle value of each correct order
   const middleValues = correctPagesOrders.map(
     (pages) => pages[((pages.length - 1) / 2).toFixed(0)],
@@ -45,7 +56,27 @@ const part1 = (rawInput: string) => {
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return;
+  const result = input.split('\n\n');
+  const pageOrders = result[0];
+  const pages = result[1];
+
+  const orders = parsePageOrders(pageOrders);
+  const pagesArr = parsePages(pages);
+
+  const inCorrectPagesOrders = pagesArr.filter(
+    (pages) => !isCorrectOrder(orders, pages),
+  );
+
+  const sortedPages = inCorrectPagesOrders.map((pages) =>
+    sortArrayBasedOnOrders(orders, pages),
+  );
+
+  //get middle value of each correct order
+  const middleValues = sortedPages.map(
+    (pages) => pages[((pages.length - 1) / 2).toFixed(0)],
+  );
+
+  return middleValues.reduce((acc, curr) => acc + curr, 0);
 };
 
 run({
@@ -87,10 +118,37 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47`,
+        expected: 123,
+      },
     ],
     solution: part2,
   },
