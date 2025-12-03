5efa5c6e156885632add02f480b1d0e37bf62020
Jochem van Weelde <jochem@heattransformers.com>
2025-12-03 17:13:00 +0100

    feat: add aoc2025 day 2 p2

--- aoc2025/src/day02/index.ts
+++ aoc2025/src/day02/index.ts
@@ -2,34 +2,6 @@ import run from "aocrunner";
 
 const parseInput = (rawInput: string) => rawInput;
 
-// function getInvalidIdsCount(instruction: string) {
-//   console.log("Instruction:", instruction);
-//   const [lowerId, higherId] = instruction.split("-");
-
-//   if (lowerId.length === higherId.length && lowerId.length % 2 === 1) {
-//     return 0;
-//   }
-
-//   if (lowerId.length % 2 === 1) {
-//     const newLowerId = Math.pow(10, lowerId.length);
-//     const newInstruction = `${newLowerId}-${higherId}`;
-//     return getInvalidIdsCount(newInstruction);
-//   }
-
-//   const leftLowerId = lowerId.slice(0, lowerId.length / 2);
-//   const rightLowerId = lowerId.slice(lowerId.length / 2);
-//   const leftHigherId = higherId.slice(0, higherId.length - rightLowerId.length);
-//   const rightHigherId = higherId.slice(higherId.length - rightLowerId.length);
-
-//   const leftDistance = Number(leftHigherId) - Number(leftLowerId);
-//   const rightDistance = Number(rightHigherId) - Number(rightLowerId);
-
-//   if (rightDistance < 0) {
-//     return leftDistance - 1;
-//   }
-//   return leftDistance;
-// }
-
 function getInvalidIds(instruction: string): number[] {
   const [lowerId, higherId] = instruction.split("-").map(Number);
 
@@ -47,6 +19,30 @@ function getInvalidIds(instruction: string): number[] {
   return result;
 }
 
+function getInvalidIdsPart2(instruction: string): number[] {
+  const [lowerId, higherId] = instruction.split("-").map(Number);
+
+  const result = new Set<number>();
+
+  for (let id = lowerId; id <= higherId; id++) {
+    const idS = id.toString();
+    for (let size = 1; size <= idS.length / 2; size++) {
+      const patterns: string[] = [];
+      if (idS.length % size !== 0) {
+        continue;
+      }
+      for (let start = 0; start <= idS.length - size; start += size) {
+        patterns.push(idS.slice(start, start + size));
+      }
+      if (patterns.every((item) => item === patterns[0])) {
+        result.add(id);
+      }
+    }
+  }
+
+  return Array.from(result);
+}
+
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
   const ranges = input.split(",");
@@ -61,8 +57,14 @@ const part1 = (rawInput: string) => {
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
+  const ranges = input.split(",");
 
-  return;
+  const result = ranges.map(getInvalidIdsPart2);
+
+  return result.reduce(
+    (prev, curr) => curr.reduce((prev, curr) => prev + curr, 0) + prev,
+    0,
+  );
 };
 
 run({
@@ -79,10 +81,16 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: "95-115",
+        expected: 210,
+      },
+      {
+        input: `11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+      1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+      824824821-824824827,2121212118-2121212124`,
+        expected: 4174379265,
+      },
     ],
     solution: part2,
   },
